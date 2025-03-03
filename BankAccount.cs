580b9f2f301d814c29d5fa6e52c667a0e7d9f85f
rezatajari <reza.tajari70@gmail.com>
2025-03-03 18:26:33 +0330

    Init

---
+++ BankAccount.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    /// <summary>
+    /// Represents a bank account. Manages account details and transactions.
+    /// </summary>
+    public class BankAccount
+    {
+        // For unique account number generator
+        private static int _accountNumberGenerator = 100;
+        public BankAccount(string accountHolderName, double initBalance)
+        {
+            var result = BankAccountValidator.ValidateBalance(initBalance);
+            if (!result.IsValid)
+            {
+                Console.WriteLine(result.Message);
+                return; // Next the runtime app without throw exception 
+            }
+
+            AccountNumber = "Account Number Is: " + _accountNumberGenerator++;
+            AccountHolderName = accountHolderName;
+            Balance = initBalance;
+        }
+
+        // Properties 
+
+        // It is readonly because we want to don't customer to set account number
+        public string AccountNumber { get; }
+        public string AccountHolderName { get; set; }
+        // It is private because a customer can't set to balance with manually from out of class
+        public double Balance { get; private set; }
+
+        // Methods
+        public void Deposit(double amount)
+        {
+        }
+
+        public void Withdraw(double amount)
+        {
+
+        }
+
+        public void CheckBalance() { }
+    }
+}
---
+++ ResponseCenter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    public class ResponseCenter
+    {
+        public bool IsValid{ get;private set; }
+        public string Message { get; private set; }
+
+        private ResponseCenter(bool isValid,string message)
+        {
+            IsValid = isValid;
+            Message = message;
+        }
+
+
+        public static ResponseCenter Success(string message = "Operation successful")
+            => new ResponseCenter(isValid:true, message);
+
+        public static ResponseCenter Fail(string message)
+            => new ResponseCenter(isValid:false, message);
+    }
+}
